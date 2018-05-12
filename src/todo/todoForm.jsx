6449f23b1e8f5f2485b0ab84e5e9dd491c128258
Cristian Araujo <araujocristian@live.com>
2018-05-12 00:39:55 -0300

    ADD button search

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -18,6 +18,11 @@ export default props => (
                 icon='plus'
                 onClick={props.handleAdd} 
             />
+            <IconButton
+                style='info'
+                icon='search'
+                onClick={props.handleSearch}
+            />
         </Grid>
     </div>
 )
